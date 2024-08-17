ed949119b28ffab08b4b68b1ca379b78b8098094
RogueFirework <70032201+roguefirework@users.noreply.github.com>
2024-08-17 14:24:41 -0700

    Update camera

--- Unity/Assets/Scripts/CameraFollow.cs
+++ Unity/Assets/Scripts/CameraFollow.cs
@@ -6,15 +6,17 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform target;
-    [SerializeField] private float smoothTime;
+    [SerializeField] private float strength;
     [SerializeField] private CameraShake shake;
     [SerializeField] private float depth = -10;
-    Vector3 currentVelocity;
     private void LateUpdate()
     {
-        Vector2 delta = target.position - transform.position;
-        
-        transform.position = Vector3.SmoothDamp(transform.position, target.position, ref currentVelocity, smoothTime);
-        transform.position = new Vector3(transform.position.x, transform.position.y, depth) + shake.currentShake;
+        Vector2 current = transform.position;
+
+        Vector2 next = (Vector2)target.transform.position * strength + current * (1 - strength);
+
+        Vector3 newPosition = new Vector3(next.x, next.y, depth) + shake.currentShake;
+        transform.position = newPosition;
     }
+    
 }
