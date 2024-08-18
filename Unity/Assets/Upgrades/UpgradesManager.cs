87744f365b0305129a0e52e19fbe930955fa9885
MaximumMaxxx <maxwellwindland@gmail.com>
2024-08-17 18:25:17 -0700

    More crap

---
+++ Unity/Assets/Upgrades/UpgradesManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradesManager : MonoBehaviour
+{
+    [SerializeField] private GameObject jetboots;
+    void Start()
+    {
+        jetboots.SetActive(true);
+    }
+}
