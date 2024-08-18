a8a6cf96edc6ae36790724cbcfab29836207d358
RogueFirework <70032201+roguefirework@users.noreply.github.com>
2024-08-18 12:02:30 -0700

    Added a shooting option

---
+++ Unity/Assets/Scripts/Spin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 spinRate;
+    private Vector3 speen;
+    public void Update()
+    {
+        speen += spinRate * Time.deltaTime;
+        this.transform.rotation = Quaternion.Euler(speen);
+    }
+}
