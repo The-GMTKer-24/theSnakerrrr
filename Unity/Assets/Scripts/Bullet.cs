a2e1a8a53161b6cf6929a11715aaed389878e9e7
RogueFirework <70032201+roguefirework@users.noreply.github.com>
2024-08-17 21:36:11 -0700

    Bullet phone home

--- Unity/Assets/Scripts/Bullet.cs
+++ Unity/Assets/Scripts/Bullet.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -6,9 +7,38 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] private GameObject particleSystem;
     [SerializeField] private float duration;
+    public static List<Bullet> bullets;
+
+    public void OnApplicationQuit()
+    {
+        bullets.Clear();
+    }
+
+    public void Awake()
+    {
+        bullets ??= new List<Bullet>();
+        bullets.Add(this);
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
+    {
+        Splode();
+    }
+
+    public void Splode()
     {
         Destroy(Instantiate(particleSystem,transform.position,quaternion.identity),duration);
         Destroy(this.gameObject);
+        bullets.Remove(this);
+    }
+
+    public void OnDestroy()
+    {
+        bullets.Remove(this);
+    }
+
+    public void OnPlayerDeath()
+    {
+        Splode();
     }
 }
