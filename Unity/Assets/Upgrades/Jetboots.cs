57b3942ec0bbf9722dcc37709489e36b9d48c742
RogueFirework <70032201+roguefirework@users.noreply.github.com>
2024-08-17 22:07:06 -0700

    Boots work now

--- Unity/Assets/Upgrades/Jetboots.cs
+++ Unity/Assets/Upgrades/Jetboots.cs
@@ -6,11 +6,20 @@ public class Jetboots : MonoBehaviour
 {
     private InputMap inputMap;
     private InputAction jet;
+    [SerializeField]
+    float jetForce = 5f;
+    [SerializeField]
+    float maxForce = 15f;
 
+    [SerializeField] private float maxFuel = 20f;
+    [SerializeField] private float fuelRegenRate = 0.5f;
+    [SerializeField]
+    private float currentFuel;
     private void Awake()
     {
         inputMap = new InputMap();
         jet = inputMap.Player.Jump;
+        currentFuel = maxFuel;
     }
 
     private void OnEnable()
@@ -23,23 +32,30 @@ public class Jetboots : MonoBehaviour
     {
         jet.Disable();
     }
-
+    
     private void FixedUpdate()
     {
-        if (jet.IsPressed())
+        if (jet.IsPressed() && currentFuel > 0)
         {
             OnJetButton();
+        }
+        else
+        {
+            if (PlayerManager.Instance.player.GetComponent<PlayerMovement>().LastGroundTime >= 0)
+                currentFuel = Math.Min(currentFuel + fuelRegenRate * Time.fixedDeltaTime, maxFuel);
         }
     }
 
     private void OnJetButton()
     {
+        if (PlayerManager.Instance.player.GetComponent<PlayerMovement>().Jumping)
+            return;
         GameObject player = PlayerManager.Instance.player;
         Rigidbody2D rigidbody2D = player.GetComponent<Rigidbody2D>();
-
-        if (rigidbody2D.velocityY < 15f)
+        currentFuel -= Time.fixedDeltaTime;
+        if (rigidbody2D.velocityY < maxForce)
         {
-            rigidbody2D.velocityY += 3f;
+            rigidbody2D.velocityY +=jetForce;
         }
     }
 }
