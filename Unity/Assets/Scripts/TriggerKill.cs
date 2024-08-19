441a7735957d48f8a611badaf5650da51908dae8
RogueFirework <70032201+roguefirework@users.noreply.github.com>
2024-08-19 12:34:55 -0700

    Add upgrades

--- Unity/Assets/Scripts/TriggerKill.cs
+++ Unity/Assets/Scripts/TriggerKill.cs
@@ -8,6 +8,7 @@ public class TriggerKill : MonoBehaviour
     [SerializeField] private String playerTag = "Player";
     [SerializeField] private bool delayKill;
     [SerializeField] private float timeToDelay;
+    [SerializeField] private float gravityIncrease = 4;
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.collider.CompareTag(playerTag))
@@ -29,7 +30,7 @@ public class TriggerKill : MonoBehaviour
         if (!delayKill)
             PlayerManager.Instance.Die();
         else
-            PlayerManager.Instance.DelayPlayerKill(timeToDelay);
+            PlayerManager.Instance.DelayPlayerKill(timeToDelay,gravityIncrease);
     }
 
 }
