b1f09e5e8cabb6b021ebc813b4484fe9c3655dd7
Plaonder <39774970+Plaonder@users.noreply.github.com>
2024-08-19 23:58:13 -0700

    Music!!!

--- Unity/Assets/Scripts/SnakeKingTrigger.cs
+++ Unity/Assets/Scripts/SnakeKingTrigger.cs
@@ -6,9 +6,18 @@ public class SnakeKingTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject snakeKing;
     [SerializeField] private Transform spawnLocation;
+    [SerializeField] private AudioSource ambientMusic;
+    [SerializeField] private AudioSource fightMusic;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (SnakeKing.Instance == null)
             Instantiate(snakeKing, spawnLocation.position, quaternion.identity);
+
+        // MUSIC
+        ambientMusic.Stop();
+        if (!fightMusic.isPlaying)
+        {
+            fightMusic.Play();
+        }
     }
 }
