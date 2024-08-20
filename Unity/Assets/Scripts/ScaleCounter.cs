cd6ed0ec2ec87b0e0a2303863ed3a32e56307993
RogueFirework <70032201+roguefirework@users.noreply.github.com>
2024-08-20 05:55:05 -0700

    Sc a le

---
+++ Unity/Assets/Scripts/ScaleCounter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class ScaleCounter : MonoBehaviour
+{
+    [SerializeField]
+    TMP_Text text;
+    public void Update()
+    {
+        if (PlayerManager.Instance)
+            text.SetText($"Scales: {PlayerManager.Instance.level1Collectables}");
+    }
+}
--- Unity/Assets/Scripts/Stats.cs
+++ Unity/Assets/Scripts/Stats.cs
@@ -9,11 +9,14 @@ public class Stats : MonoBehaviour
     [SerializeField] private TMP_Text time;
 
     [SerializeField] private TMP_Text deaths;
+
+    [SerializeField] private TMP_Text scales;
     // Start is called before the first frame update
     void Start()
     {
         time.SetText($"{TimeSpan.FromSeconds(StatPasser.Instance.time):h\\:mm\\:ss\\.f}");
         deaths.SetText($"Deaths: {StatPasser.Instance.deaths}");
+        scales.SetText($"Scales: {StatPasser.Instance.scales}/12");
         Destroy(StatPasser.Instance.gameObject);
     }
 
